5cfac16473bbccfebc08b9ec6413296c41ec3305
richardktran <richardktran.dev@gmail.com>
2023-12-14 00:31:41 +0700

    create match events table

---
+++ database/migrations/2023_12_13_171954_create_match_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('match_events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('match_id')->nullable();
+            $table->unsignedBigInteger('player_id')->nullable();
+            $table->unsignedBigInteger('assistant_player_id')->nullable();
+            $table->unsignedBigInteger('player_in_id')->nullable();
+            $table->unsignedBigInteger('player_out_id')->nullable();
+            $table->string('event_type');
+            $table->integer('event_minute');
+
+            $table->foreign('match_id')->constrained()->references('id')->on('matches')->cascadeOnDelete();
+            $table->foreign('player_id')->constrained()->references('id')->on('players')->cascadeOnDelete();
+            $table->foreign('assistant_player_id')->constrained()->references('id')->on('players')->cascadeOnDelete();
+            $table->foreign('player_in_id')->constrained()->references('id')->on('players')->cascadeOnDelete();
+            $table->foreign('player_out_id')->constrained()->references('id')->on('players')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('match_events');
+    }
+};
